e1d1f4306018ead5a334f962f9b0a62dfe274b3e
Kandy-Hamisi <hkandy2000@gmail.com>
2025-10-15 15:14:52 +0300

    Implement project stats, preferences and project deletion

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,48 @@
-import {StyleSheet, Text, View} from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createSettingsStyles } from "@/assets/styles/settings.styles";
+import useTheme from "@/hooks/useTheme";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import ProgressStats from "@/components/ProgressStats";
+import Preferences from "@/components/Preferences";
+import DangerZone from "@/components/DangerZone";
 
 export default function Settings() {
-    return (
-        <View>
-            <Text>Settings</Text>
+  const { colors } = useTheme();
+  const settingsStyles = createSettingsStyles(colors);
+
+  return (
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingsStyles.container}
+    >
+      <SafeAreaView style={settingsStyles.safeArea}>
+        {/*  HEADER */}
+        <View style={settingsStyles.header}>
+          <View style={settingsStyles.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={settingsStyles.iconContainer}
+            >
+              <Ionicons name="settings" size={28} color="#ffffff" />
+            </LinearGradient>
+            <Text style={settingsStyles.title}>Settings</Text>
+          </View>
         </View>
-    )
+
+        <ScrollView
+          style={settingsStyles.scrollView}
+          contentContainerStyle={settingsStyles.content}
+          showsVerticalScrollIndicator={false}
+        >
+          <ProgressStats />
+          <Preferences />
+          <DangerZone />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
+  );
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
